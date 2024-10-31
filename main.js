9ce6c21e0c40f56f4a6181c7f63abfbbb003a13d
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2024-10-31 12:03:04 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 function criarCartao(pergunta, resposta){
     let conteudo = document.getElementById("conteudo")
-    console.log(conteudo)
+    let cartao = document.createElement('article')
+    cartao.classList = 'cartao'
+
+    cartao.innerHTML = 'TESTANDO O INNER'
+
+    conteudo.appendChild(cartao)
 }
 
 criarCartao(1,2)
